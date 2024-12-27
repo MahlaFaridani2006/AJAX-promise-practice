b5f2adc8163e74c132a70c7501470ade1b67ad99
MahlaFaridani2006 <mahlafaridani@gmail.com>
2024-12-27 11:48:21 -0800

    version-0

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+function AJAX() {
+    return new Promise(function (myResolve, myReject) {
+        const http = new XMLHttpRequest()
+        http.onreadystatechange = function () {
+            if (this.status === 200, this.readyState === 4) {
+                myResolve(this.responseText)
+            }
+        }
+        http.open('GET', 'https://timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam', true);
+        http.send()
+    })
+}
+async function callAJAX() {
+      return document.getElementById('show-time').innerText = await AJAX()
+}
+
+setInterval(() => {
+    callAJAX()
+}, 10000);
